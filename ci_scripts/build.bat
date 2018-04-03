1de9832a75cc44debacf130f522026f8279898d1
Yannan Wang <yannw@microsoft.com>
2018-04-03 12:57:50 +0800

    Use our repo build.bat instead of cntk repo file

---
+++ ci_scripts/build.bat
@@ -0,0 +1,23 @@
+setlocal
+
+cd /d "%~dp0"
+set PYTHONPATH=%CD%\..
+set PATH=%CD%\..;%CD%\..\..\..\x64\Release;%PATH%
+
+sphinx-apidoc.exe ..\cntk --module-first --separate --no-toc --output-dir=. --force ^
+  ..\cntk\cntk_py.py ^
+  ..\cntk\conftest.py ^
+  ..\cntk\tests ^
+  ..\cntk\debugging\tests ^
+  ..\cntk\eval\tests ^
+  ..\cntk\internal ^
+  ..\cntk\io\tests ^
+  ..\cntk\layers\tests ^
+  ..\cntk\learners\tests ^
+  ..\cntk\logging\tests ^
+  ..\cntk\losses\tests ^
+  ..\cntk\metrics\tests ^
+  ..\cntk\ops\tests ^
+  ..\cntk\train\tests
+
+if errorlevel 1 exit /b 1
